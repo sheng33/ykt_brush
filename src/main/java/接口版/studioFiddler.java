368480f17b7c8c63aaaf16dd8e419e5ed2fe8257
1305552980@github.com <1305552980@qq.com>
2019-10-23 12:48:37 +0800

    提交

--- src/main/java/接口版/studioFiddler.java
+++ src/main/java/接口版/studioFiddler.java
@@ -17,82 +17,40 @@ import java.util.*;
 
 
 public class studioFiddler {
-    //导入资源文件
-    static ResourceBundle resource = ResourceBundle.getBundle("ZhiJiaoYunApi");
-    static Map<String,String> headers = new HashMap<>();
-    static Map<String,String> data = new LinkedHashMap<>();
-    static List<Course> courseList = new ArrayList<>();
-    static List<ProcessList> processLists = new ArrayList<>();
-    static List<TopicByModuleId> topicByModuleIdList = new ArrayList<>();
-    static List<CellByTopicId> cellByTopicIdList = new ArrayList<>();
-    static List<child_DIrectory>  child_dIrectoryList = new ArrayList<>();
-    //课程ID，修改此项，可选择需要刷的课程
-    static int course = 0;
-    static String cookieStr = "";
-    static int xunhuan = 0;
     /**
-     *  登录方法
-     * @param driver
-     * @return cookieStr
-     * @throws Exception
+     * 导入资源文件
      */
-    public static String login(WebDriver driver) throws Exception {
-        String cookieStr = "";
-        driver.get("https://zjy2.icve.com.cn/portal/login.html");
-        WebElement userName = driver.findElement(By.xpath(".//*[@name='userName']"));
-        userName.sendKeys("173053432");
-        WebElement userPassword = driver.findElement(By.name("userPassword"));
-        userPassword.sendKeys("123456ABCDa");
-        //获得验证码
-        String Code = ImgCode.getCode(driver);
-//        System.out.println(Code);
-        WebElement webCode = driver.findElement(By.name("photoCode"));
-        webCode.sendKeys(Code);
-        driver.findElement(By.id("btnLogin")).click();
-        Thread.sleep(6000);
-        Set<Cookie> cookie = driver.manage().getCookies();
-        for(Cookie value:cookie){
-            if(value.toJson().containsValue("auth")){
-                cookieStr +="auth="+value.toJson().get("value")+";";
-            }
-            if(value.toJson().containsValue("Hm_lvt_a3821194625da04fcd588112be734f5b")){
-                cookieStr +="Hm_lvt_a3821194625da04fcd588112be734f5b="+value.toJson().get("value")+";";
-            }
-            if(value.toJson().containsValue("Hm_lpvt_a3821194625da04fcd588112be734f5b")){
-                cookieStr +="Hm_lpvt_a3821194625da04fcd588112be734f5b="+value.toJson().get("value")+";";
-            }
-            if(value.toJson().containsValue("acw_tc")){
-                cookieStr +="acw_tc="+value.toJson().get("value")+";";
-            }
-            if(value.toJson().containsValue("verifycode")){
-                cookieStr +="verifycode="+value.toJson().get("value")+";";
-            }
-            if(value.toJson().containsValue("_bl_uid")){
-                cookieStr +="_bl_uid="+value.toJson().get("value")+";";
-            }
-        }
-        return cookieStr;
-    }
+    private static ResourceBundle resource = ResourceBundle.getBundle("ZhiJiaoYunApi");
+    /**
+     * post请求header头部信息
+     */
+    private static Map<String,String> headers = new HashMap<>();
+    /** post请求body主体信息 */
+    private static Map<String,String> data = new LinkedHashMap<>();
+    /**课程列表 */
+    private static List<Course> courseList = new ArrayList<>();
+    /**课程章节列表 */
+    private static List<ProcessList> processLists = new ArrayList<>();
+    /**章节信息列表 */
+    private static List<TopicByModuleId> topicByModuleIdList = new ArrayList<>();
+    /**章节详情列表 */
+    private static List<CellByTopicId> cellByTopicIdList = new ArrayList<>();
+    /**课件详情信息列表 */
+    private static List<child_DIrectory>  child_dIrectoryList = new ArrayList<>();
+    /**课程ID，修改此项，可选择需要刷的课程 */
+    private static int course = 0;
+    /**Cookie值 */
+    private static String cookieStr = "";
+    /**防止死循环 */
+    private static int xunhuan = 0;
     public static void main(String[] args) throws Exception {
         System.out.print("输入cookie:");
         Scanner sc = new Scanner(System.in);
         cookieStr = sc.nextLine();
         headers.put("Cookie",cookieStr);
         System.out.println("欢迎您："+getUserInfo());
-        //请求头部信息
-//        System.setProperty("webdriver.chrome.driver","D:\\xt\\chromedriver.exe");
-//        //无头参数
-//        ChromeOptions chromeOptions=new ChromeOptions();
-//        chromeOptions.addArguments("-headless");
-//        //初始化浏览器名为driver
-//        WebDriver driver = new ChromeDriver(chromeOptions);
         try {
-            //登录并获取cookie值
-//          String cookieStr = login(driver);
-//          headers.put("Cookie",cookieStr);
             JSONObject course_Json = Post.send_Object(headers,resource.getString("getLearnningCourseList"));
-
-
             //解析Json数据创建得到课程数据
             try{
                 if(course_Json.get("code").equals(1)){
@@ -103,18 +61,12 @@ public class studioFiddler {
                     for(Course course:courseList){
                         String str = "Id:"+temp_count+"\t"+"课程名："+course.getCourseName().replace(" ","").replace("1","一");
                         temp_number = str.getBytes("GBK").length;
-//                        System.out.println("length:"+temp_number);
-//                        temp_number = str.getBytes("UTF-8").length;
-//                        System.out.println("length:"+temp_number);
                         while ((50-temp_number++)>0){
                             str+=" ";
                         }
                         System.out.printf(str);
                         str = "任课老师:"+course.getAssisTeacherName();
                         temp_number = str.getBytes("GBK").length;
-//                        System.out.println("length:"+temp_number);
-//                        temp_number = str.getBytes("UTF-8").length;
-//                        System.out.println("length:"+temp_number);
                         while ((25-temp_number++)>0){
                             str+=' ';
                         }
@@ -148,12 +100,10 @@ public class studioFiddler {
                     JSONObject jsonObject2 = getCellByTopicId(resource.getString("getCellByTopicId"),data);
                     cellByTopicIdList = JsonObject_course.getObject_CellByTopicId(jsonObject2.toString());
                     for(int cellCount=0;cellCount<cellByTopicIdList.size();cellCount++){
-//                    System.out.println("bbb:"+cellByTopicIdList.get(cellCount).getChildNodeLists());
                         if(cellByTopicIdList.get(cellCount).getChildNodeLists()==null){
                             for(int childCount=0;childCount<cellByTopicIdList.size();childCount++){
                                 if(cellByTopicIdList.get(cellCount).getStuCellPercent() == 100||cellByTopicIdList.get(cellCount).getCategoryName().equals("文档")
                                 ||cellByTopicIdList.get(cellCount).getCategoryName().equals("链接")){
-//                            System.out.println("完成:"+cellByTopicIdList.get(cellCount).getChildNodeLists().get(childCount).getCellName());
                                     continue;
                                 }
 
@@ -165,7 +115,6 @@ public class studioFiddler {
                         }else{
                             for(int childCount=0;childCount<cellByTopicIdList.get(cellCount).getChildNodeLists().size();childCount++){
                                 if(cellByTopicIdList.get(cellCount).getChildNodeLists().get(childCount).getStuCellFourPercent() == 100){
-//                            System.out.println("完成:"+cellByTopicIdList.get(cellCount).getChildNodeLists().get(childCount).getCellName());
                                     continue;
                                 }
                                 boolean pd = true;
@@ -187,10 +136,28 @@ public class studioFiddler {
 //            driver.close();
         }
     }
+
+    /**
+     * 得到用户姓名
+     * @return
+     * @throws IOException
+     * @throws ParseException
+     */
     public static String getUserInfo() throws IOException, ParseException {
         JSONObject jsonObject = Post.send_Body_Object(headers,data,resource.getString("getUserInfo"));
         return jsonObject.get("disPlayName").toString();
     };
+
+    /**
+     * 用于没有childNodeLists列表的课程
+     * @param processCount
+     * @param cellCount
+     * @param childCount
+     * @return
+     * @throws IOException
+     * @throws ParseException
+     * @throws InterruptedException
+     */
     public static boolean funtion1(int processCount,int cellCount,int childCount) throws IOException, ParseException, InterruptedException {
         data.clear();
         data.put("courseOpenId",courseList.get(course).getCourseOpenId());
@@ -198,21 +165,14 @@ public class studioFiddler {
         data.put("cellId",cellByTopicIdList.get(cellCount).getId());
         data.put("flag","s");
         data.put("moduleId",processLists.get(0).getModuleLists().get(processCount).getId());
-//                      for(String key:data.keySet()){//keySet获取map集合key的集合  然后在遍历key即可
-//                         String value = data.get(key).toString();//
-//                         System.out.println(key+":"+value);
-//                      }
         JSONObject jsonObject3 = viewDirectory(resource.getString("viewDirectory"),data);
         child_dIrectoryList = JsonObject_course.child_dIrectoryList(jsonObject3.toString());
-//                      prints(courseList,processLists,topicByModuleIdList,cellByTopicIdList,child_dIrectoryList);
         Thread.sleep(1500);
         data.clear();
         data.put("courseOpenId",courseList.get(course).getCourseOpenId());
         data.put("openClassId",courseList.get(course).getOpenClassId());
         data.put("cellId",child_dIrectoryList.get(0).getCellId());
         data.put("cellLogId",child_dIrectoryList.get(0).getCellLogId());
-//                      System.out.println("child_dIrectoryList:"+child_dIrectoryList.get(0));
-//                      System.out.println("打开文档:"+child_dIrectoryList.get(0).getCellName());
         System.out.println("当前执行:"+cellByTopicIdList.get(cellCount).getCellName());
 
         if(cellByTopicIdList.get(cellCount).getCategoryName().equals("视频")){
@@ -226,6 +186,17 @@ public class studioFiddler {
             return  brush_ppt(processCount,cellCount,childCount);
         }
     }
+
+    /**
+     * 用于有childNodeLists列表的课程
+     * @param processCount
+     * @param cellCount
+     * @param childCount
+     * @return
+     * @throws IOException
+     * @throws ParseException
+     * @throws InterruptedException
+     */
     public static boolean funtion(int processCount,int cellCount,int childCount) throws IOException, ParseException, InterruptedException {
         data.clear();
         data.put("courseOpenId",courseList.get(course).getCourseOpenId());
@@ -233,21 +204,14 @@ public class studioFiddler {
         data.put("cellId",cellByTopicIdList.get(cellCount).getChildNodeLists().get(childCount).getId());
         data.put("flag","s");
         data.put("moduleId",processLists.get(0).getModuleLists().get(processCount).getId());
-//                      for(String key:data.keySet()){//keySet获取map集合key的集合  然后在遍历key即可
-//                         String value = data.get(key).toString();//
-//                         System.out.println(key+":"+value);
-//                      }
         JSONObject jsonObject3 = viewDirectory(resource.getString("viewDirectory"),data);
         child_dIrectoryList = JsonObject_course.child_dIrectoryList(jsonObject3.toString());
-//                      prints(courseList,processLists,topicByModuleIdList,cellByTopicIdList,child_dIrectoryList);
         Thread.sleep(1500);
         data.clear();
         data.put("courseOpenId",courseList.get(course).getCourseOpenId());
         data.put("openClassId",courseList.get(course).getOpenClassId());
         data.put("cellId",child_dIrectoryList.get(0).getCellId());
         data.put("cellLogId",child_dIrectoryList.get(0).getCellLogId());
-//                      System.out.println("child_dIrectoryList:"+child_dIrectoryList.get(0));
-//                      System.out.println("打开文档:"+child_dIrectoryList.get(0).getCellName());
         System.out.println("当前执行:"+cellByTopicIdList.get(cellCount).getChildNodeLists().get(childCount).getCellName());
         if(cellByTopicIdList.get(cellCount).getChildNodeLists().get(childCount).getCategoryName().equals("视频")){
             return brush_video(processCount,cellCount,childCount);
@@ -259,6 +223,16 @@ public class studioFiddler {
             return  brush_ppt(processCount,cellCount,childCount);
         }
     }
+
+    /**
+     * 获得子课件完成度
+     * @param processCount
+     * @param cellCount
+     * @param childCount
+     * @return
+     * @throws IOException
+     * @throws ParseException
+     */
     public static int getPercent(int processCount,int cellCount,int childCount) throws IOException, ParseException {
         data.clear();
         data.put("courseOpenId",courseList.get(course).getCourseOpenId());
@@ -277,6 +251,16 @@ public class studioFiddler {
         JSONObject jsonObject3 = viewDirectory(resource.getString("viewDirectory"),data);
         return jsonObject3.getInt("cellPercent");
     }
+
+    /**
+     * 刷ppt、文档
+     * @param processCount
+     * @param cellCount
+     * @param childCount
+     * @return
+     * @throws IOException
+     * @throws ParseException
+     */
     public static Boolean brush_ppt(int processCount,int cellCount,int childCount) throws IOException, ParseException {
         int j;
         JSONObject jsonObject4 = null;
@@ -286,10 +270,6 @@ public class studioFiddler {
             for (j=0;j<=i;j++){
                 data.put("studyNewlyPicNum",String.valueOf(j));
                 data.put("token",String.valueOf(child_dIrectoryList.get(0).getGuIdToken()));
-//                        for(String key:data.keySet()){//keySet获取map集合key的集合  然后在遍历key即可
-//                            String value = data.get(key).toString();
-//                            System.out.println(key+":"+value);
-//                        }
                 jsonObject4 = stuProcessCellLog(resource.getString("stuProcessCellLog"),data);
                 if((jsonObject4.getString("code").equals("-1"))&&i==j&&i==child_dIrectoryList.get(0).getPageCount()){
                     System.out.println("正在执行...当前完成度:"+getPercent(processCount, cellCount, childCount));
@@ -309,6 +289,17 @@ public class studioFiddler {
         }
         return true;
     }
+
+    /**
+     * 刷视频
+     * @param processCount
+     * @param cellCount
+     * @param childCount
+     * @return
+     * @throws IOException
+     * @throws ParseException
+     * @throws InterruptedException
+     */
     public static Boolean brush_video(int processCount,int cellCount,int childCount) throws IOException, ParseException, InterruptedException {
         int j;
         JSONObject jsonObject4 = null;
@@ -351,26 +342,70 @@ public class studioFiddler {
         }
         return true;
     }
+
+    /**
+     * 得到课程章节列表
+     * @param url
+     * @param data
+     * @return
+     * @throws IOException
+     * @throws ParseException
+     */
     public static JSONObject getProcessList(String url,Map<String,String> data) throws IOException, ParseException {
         JSONObject jsonObject = Post.send_Body_Object(headers,data,url);
-//        System.out.println("getProcessList："+jsonObject.toString());
         return jsonObject;
     }
+
+    /**
+     * 得到课程章节下子课件
+     * @param url
+     * @param data
+     * @return
+     * @throws IOException
+     * @throws ParseException
+     */
     public static JSONObject getTopicByModuled(String url,Map<String,String> data) throws IOException, ParseException {
         JSONObject jsonObject = Post.send_Body_Object(headers,data,url);
 //        System.out.println("getTopicByModuled："+jsonObject.toString());
         return jsonObject;
     }
+
+    /**
+     * 得到课程列表
+     * @param url
+     * @param data
+     * @return
+     * @throws IOException
+     * @throws ParseException
+     */
     public static JSONObject getCellByTopicId(String url,Map<String,String> data) throws IOException, ParseException {
         JSONObject jsonObject = Post.send_Body_Object(headers,data,url);
 //        System.out.println("getCellByTopicId："+jsonObject.toString());
         return jsonObject;
     }
+
+    /**
+     * 得到课件信息
+     * @param url
+     * @param data
+     * @return
+     * @throws IOException
+     * @throws ParseException
+     */
     public static JSONObject viewDirectory(String url,Map<String,String> data) throws IOException, ParseException {
         JSONObject jsonObject = Post.send_Body_Object(headers,data,url);
 //        System.out.println("viewDirectory："+jsonObject.toString());
         return jsonObject;
     }
+
+    /**
+     * 请求课件进度
+     * @param url
+     * @param data
+     * @return
+     * @throws IOException
+     * @throws ParseException
+     */
     public static JSONObject stuProcessCellLog(String url,Map<String,String> data) throws IOException, ParseException {
         JSONObject jsonObject = Post.send_Body_Object(headers,data,url);
 //        System.out.println("stuProcessCellLog："+jsonObject.toString());
